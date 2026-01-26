04a6e33c3e43fca5831fa338d8dfbbdb7825cc03
angelo <angelo.sicssu0725@jala.university>
2026-01-26 11:28:23 -0400

    feat: add header

---
+++ src/components/contact.tsx
@@ -0,0 +1,25 @@
+import { Dialog, DialogTrigger, DialogContent, DialogHeader, DialogTitle, DialogDescription, DialogFooter, DialogClose } from "./ui/dialog"
+import { Button } from "./ui/button"
+import { Label } from "./ui/label"
+import { Input } from "./ui/input"
+
+export const Contact = () => {
+    return(
+        <Dialog>
+        <DialogTrigger asChild>
+            <Button variant="forHeader">Contato</Button>
+        </DialogTrigger>
+        <DialogContent>
+            <DialogHeader>
+            <DialogTitle>Formas de Contato</DialogTitle>
+            <DialogDescription>
+                Olá, tudo bem? Para entrar em contato com a secretaria <br/>
+                Telefone: (92) 99173-5298 <br/>
+                Email: secretarianibgc@gmail.com <br/>
+                Instagram: nibgrandecircular <br/>
+            </DialogDescription>
+            </DialogHeader>
+        </DialogContent>
+        </Dialog>
+    )
+}
---
+++ src/components/header.tsx
@@ -0,0 +1,29 @@
+import Image from "next/image";
+import { Button } from "./ui/button"
+import { FaYoutube, FaInstagram, FaSpotify } from "react-icons/fa";
+import { Contact } from "./contact";
+
+export const Header = () => {
+    return (
+        <div className="flex justify-between items-center bg-linear-to-r from-[#1D0072] to-[#1b238e] px-5 py-3">
+            <div>
+                <Image src="/images/nibGrandeCircular.png" alt="Logo NIB Grande Circular" width={100} height={100}/>
+            </div>
+            <nav className="flex items-center gap-10">
+                <ul className="flex items-center gap-10">
+                    <div className="flex gap-10 text-white font-bold text-xl uppercase">
+                        <li><a className="relative after:absolute after:left-0 after:-bottom-1 after:h-[4px] after:w-0 after:bg-[#F10078]  after:transition-all after:duration-400 hover:after:w-full">Quem Somos</a></li>
+                        <li><a className="relative after:absolute after:left-0 after:-bottom-1 after:h-[4px] after:w-0 after:bg-[#F10078]  after:transition-all after:duration-400 hover:after:w-full">Meditação</a></li>
+                        <li><a className="relative after:absolute after:left-0 after:-bottom-1 after:h-[4px] after:w-0 after:bg-[#F10078]  after:transition-all after:duration-400 hover:after:w-full">Eventos</a></li>
+                    </div>
+                    <div className="flex gap-5">
+                        <li className="cursor-pointer"><FaYoutube size={30} color="#FFF"/></li>
+                        <li className="cursor-pointer"><FaInstagram size={30} color="#FFF"/></li>
+                        <li className="cursor-pointer"><FaSpotify size={30} color="#FFF"/></li>
+                    </div>
+                </ul>
+                <Contact/>
+            </nav>
+        </div>
+    )
+}
