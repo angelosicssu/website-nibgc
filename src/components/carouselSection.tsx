385880394e50e63c6adeed7eac16bb53a1bd6f4b
angelo <angelo.sicssu0725@jala.university>
2026-02-03 11:22:51 -0400

    feat: completed carousel

--- src/components/carouselSection.tsx
+++ src/components/carouselSection.tsx
@@ -4,6 +4,28 @@ import * as React from "react"
 import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "./ui/carousel"
 import { Card, CardContent } from "./ui/card"
 import Autoplay from "embla-carousel-autoplay"
+import Image from "next/image"
+
+const cards = [
+    {
+        id: 1,
+        img: "/images/culto.png",
+        alt: "NIB Grande Circular",
+        bg: "bg-gradient-to-r from-[#4A62FF] to-[#0900B2]",
+    },
+    {
+        id: 2,
+        img: "/images/novaJovens.png",
+        alt: "NIB Nova Jovens",
+        bg: "bg-gradient-to-r from-[#000000] to-[#676767]",
+    },
+    {
+        id: 3,
+        img: "/images/novaTeens.png",
+        alt: "NIB Nova Teens",
+        bg: "bg-gradient-to-r from-[#FF9501] to-[#FF9500]",
+    },
+]
 
 export const CarouselSection = () => {
     const plugin = React.useRef(
@@ -19,18 +41,16 @@ export const CarouselSection = () => {
             <div>
                 <Carousel
                     plugins={[plugin.current]}
-                    className="w-full max-w-[10rem] sm:max-w-xs"
+                    className="w-full max-w-[28rem]"
                 >
                     <CarouselContent>
-                        {Array.from({ length: 5 }).map((_, index) => (
-                            <CarouselItem key={index}>
-                                <div className="p-1">
-                                    <Card>
-                                        <CardContent className="flex aspect-square items-center justify-center p-6">
-                                            <span className="text-4xl font-semibold">{index+1}</span>
-                                        </CardContent>
-                                    </Card>
-                                </div>
+                        {cards.map((card) => (
+                            <CarouselItem key={card.id} className="flex justify-center">
+                                <Card className={`${card.bg} w-[420px] h-[430px]`}>
+                                <CardContent className={`flex h-full items-center justify-center`}>
+                                        <Image src={card.img} alt={card.alt} width={300} height={80}/>
+                                    </CardContent>
+                                </Card>
                             </CarouselItem>
                         ))}
                     </CarouselContent>
